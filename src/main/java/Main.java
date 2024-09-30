0828a84d25ba4b5ca8c102bb4ec1350bea9aff8f
Matii <matiiasdelvalle@gmail.com>
2024-09-30 13:22:27 -0300

    Agregado de consultas en el main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,15 +21,50 @@ public class Main {
 
         EstudianteCarreraRepositoryImp estudiantecarreraRepository = new EstudianteCarreraRepositoryImp(em);
         EstudianteRepositoryImp estudianteRepository = new EstudianteRepositoryImp(em);
-        CarreraRepositoryImp carreraRepositoryImp = new CarreraRepositoryImp(em);
 
+        System.out.println("   ----------------------------------------   ");
 
+        List<Estudiante> estudiantes1 = new ArrayList<>();
+        estudiantes1 = estudianteRepository.getEstudiantesByGenero("Masculino");
+
+        for (Estudiante estudiante : estudiantes1) {
+            estudiante.toString();
+        }
+
+        System.out.println("   ----------------------------------------   ");
+
+        Estudiante estudiantes2 = estudianteRepository.getEstudianteByLegajo(1001);
+        estudiantes2.toString();
+
+        System.out.println("   ----------------------------------------   ");
+
+        //Chequear porque solo muestra la consulta sql
         List<Estudiante> estudiantes = new ArrayList<>();
         Carrera c10 = new Carrera( "Química");
         estudiantes = estudiantecarreraRepository.getEstudiantesByCarreraFiltroCiudad(c10,"Bahía Blanca");
 
         for (Estudiante est : estudiantes) {
             System.out.println(est);
+        }
+
+        System.out.println("   ----------------------------------------   ");
+
+        //Chequear este metodo porque no anda
+        List<Carrera> carreras = new ArrayList<>();
+        carreras = estudiantecarreraRepository.getCarrerasInscriptosOrdenadas();
+
+        for (Carrera c : carreras) {
+            System.out.println(c);
+        }
+
+        System.out.println("   ----------------------------------------   ");
+
+        //Chequear este metodo porque no anda
+        List<Carrera> carreras1 = new ArrayList<>();
+        carreras1 = estudiantecarreraRepository.reporteCarreras();
+
+        for (Carrera c : carreras1) {
+            System.out.println(c);
         }
 
         transaction.commit();
